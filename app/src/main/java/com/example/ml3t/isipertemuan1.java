dfde435cdedae3df178d1ca762844d66c293776e
ibnuhaldun12 <ibnuhaldun12@gmail.com>
2021-05-04 10:01:46 +0700

    mengisi materi 1

---
+++ app/src/main/java/com/example/ml3t/isipertemuan1.java
@@ -0,0 +1,14 @@
+package com.example.ml3t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class isipertemuan1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isipertemuan1);
+    }
+}
